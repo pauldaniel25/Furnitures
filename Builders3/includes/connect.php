c4f5458cce2ca94a994bfe0061dd7c3f3221cdac
NoctruneJohnH05 <168771057+NoctruneJohnH05@users.noreply.github.com>
2024-12-01 22:40:47 -0800

    Update connect.php

--- Builders3/includes/connect.php
+++ Builders3/includes/connect.php
@@ -3,7 +3,7 @@
 $host="localhost";
 $user="root";
 $pass="";
-$db="furniture3";
+$db="furniture";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
